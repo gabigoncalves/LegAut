455c9ef1b951248879fe85a0de96b63406ec6e2f
gabriela.goncalves <gabriela.goncalves@gitlab.globalhitss.com.br>
2020-03-10 17:29:39 -0300

    inclusão da logo e do texto

--- legaut/src/App.js
+++ legaut/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div class="infoBar">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="body">
         <p>
-          Teste ok! uhu!!!!!!!!!!!!!
+        Johnnie Walker comprou uma Ferrari por R$ 1.000.000,00 de seu conhecido Jack Daniels.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
